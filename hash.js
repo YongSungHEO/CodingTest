88131a897dc90fca530d73b1ce6b2d67436e125a
YongSung HEO <ysheo94@gmail.com>
2019-09-03 15:36:41 +0900

    Camouflage matter is solved.

--- hash.js
+++ hash.js
@@ -18,3 +18,24 @@ function solution(participant, completion) {
         }
     }
 }
+
+
+// 위장
+function solution(clothes) {
+    var answer = 1;
+    let map = new Map();
+
+    for (let cloth of clothes) {
+        if (!map.has(cloth[1])) {
+            map.set(cloth[1], 1);
+        } else {
+            map.set(cloth[1], map.get(cloth[1]) + 1);
+        }
+    }
+
+    for (let value of map.values()) {
+        answer *= (value + 1);
+    }
+
+    return answer - 1;
+}
