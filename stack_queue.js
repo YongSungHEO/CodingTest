b509c13ef177372fa1f29f43e8adf171ce0b24d8
YongSung HEO <ysheo94@gmail.com>
2019-09-05 19:56:42 +0900

    Truck that cross the bridge matter is solved.

--- stack_queue.js
+++ stack_queue.js
@@ -19,7 +19,6 @@ function solution(priorities, location) {
 
     return answer + 1;
 }
-
 console.log(solution([2, 1, 3, 2], 2));
 console.log(solution([1, 1, 9, 1, 1, 1], 0));
 
@@ -44,7 +43,6 @@ function solution(heights) {
 
     return answer;
 }
-
 console.log(solution([6,9,5,7,4]));
 console.log(solution([3,9,9,3,5,7,2]));
 console.log(solution([1,5,3,6,7,6,5]));
@@ -70,5 +68,32 @@ function solution(arrangement) {
     
     return answer;
 }
-
 console.log(solution('()(((()())(())()))(())'));
+
+
+// 다리를 지나는 트럭(Level2)
+function solution(bridge_length, weight, truck_weights) {
+    var answer = 0;
+    let ongoingQueue = [];
+    let timerQueue = [];
+    let carWeight = 0;
+
+    while (truck_weights.length || ongoingQueue.length) {
+        answer++;
+        if (carWeight + truck_weights[0] <= weight) {
+            carWeight += truck_weights[0];
+            ongoingQueue.push(truck_weights.shift());
+            timerQueue.push(0);
+        }
+        timerQueue = timerQueue.map(val => { return ++val; });
+        if (timerQueue[0] === bridge_length) {
+            timerQueue.shift();
+            carWeight -= ongoingQueue.shift();
+        }
+    }
+
+    return answer + 1;
+}
+console.log(solution(2, 10, [7, 4, 5, 6]));
+console.log(solution(100, 100, [10]));
+console.log(solution(100, 100, [10, 10, 10, 10, 10, 10, 10, 10, 10, 10]));
