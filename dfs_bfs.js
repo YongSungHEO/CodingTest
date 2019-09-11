ae9011862d90d77d6c424b72e278177d4505f7cf
YongSung HEO <ysheo94@gmail.com>
2019-09-11 13:42:04 +0900

    Network matter is solved.

--- dfs_bfs.js
+++ dfs_bfs.js
@@ -16,3 +16,29 @@ function recursive(numbers, target, index, sum) {
             + recursive(numbers, target, index + 1, sum - numbers[index]);
 }
 console.log(solution([1, 1, 1, 1, 1], 3));
+
+
+// 네트워크
+function solution(n, computers) {
+    var answer = 0;
+    let visited = [];
+    
+    for (let i=0; i<n; i++) {
+        if (!visited[i]) {
+            answer++;
+            recursive(i, n, computers);
+            function recursive(index, n, computers) {
+                visited[index] = 1;
+                for (let j=1; j<n; j++) {
+                    if (!visited[j] && computers[index][j] === 1) {
+                        recursive(j, n, computers);
+                    }
+                }
+            }
+        }
+    }
+
+    return answer;
+}
+console.log(solution(3, [[1, 1, 0], [1, 1, 0], [0, 0, 1]]));
+console.log(solution(3, [[1, 1, 0], [1, 1, 1], [0, 1, 1]]));
