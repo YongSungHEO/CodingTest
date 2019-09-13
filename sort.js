6f036b0d91b143fef67b786e088f35b88860fa1f
YongSung HEO <ysheo94@gmail.com>
2019-09-13 22:51:56 +0900

    Find max number matter is solved.

--- sort.js
+++ sort.js
@@ -11,3 +11,20 @@ function solution(array, commands) {
     return answer;
 }
 console.log(solution([1, 5, 2, 6, 3, 7, 4], [[2, 5, 3], [4, 4, 1], [1, 7, 3]]));
+
+
+// 가장 큰 수
+function solution(numbers) {
+    var answer = '';
+
+    numbers = numbers.map(number => {
+        return number.toString();
+    });
+    answer = numbers.sort((a, b) => {
+        return (b + a) - (a + b);
+    }).join('');
+
+    return numbers[0] === '0' ? '0' : answer;
+}
+console.log(solution([6, 10, 2]));
+console.log(solution([3, 30, 34, 5, 9]));
