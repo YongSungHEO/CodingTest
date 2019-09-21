f9b7833938ea734c21c9a42e3377c3f85063510d
YongSung HEO <ysheo94@gmail.com>
2019-09-21 18:54:44 +0900

    Find day matter is solved.

--- practice.js
+++ practice.js
@@ -12,3 +12,13 @@ function solution(s) {
 }
 console.log(solution('abcde'));
 console.log(solution('qwer'));
+
+
+// 2016년
+function solution(a, b) {
+    var answer = '';
+    let array = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+    let d = new Date('2016-' + a.toString() + '-' + b.toString());
+    answer = array[d.getDay()];
+    return answer;
+}
