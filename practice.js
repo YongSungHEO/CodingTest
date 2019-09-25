26567923bb0db1f88e2249db5abece93543d4d9e
YongSung HEO <ysheo94@gmail.com>
2019-09-25 13:56:48 +0900

    Find sum of divisor matter is solved.

--- practice.js
+++ practice.js
@@ -259,3 +259,18 @@ function solution(s, n) {
 console.log(solution('AB', 1));
 console.log(solution('z', 1));
 console.log(solution('a B z', 4));
+
+
+// 약수의 합
+function solution(n) {
+    var answer = 0;
+
+    for (let i=1; i<=n; i++) {
+        if (n % i === 0) {
+            answer += i;
+        }
+    }
+    return answer;
+}
+console.log(solution(12));
+console.log(solution(5));
